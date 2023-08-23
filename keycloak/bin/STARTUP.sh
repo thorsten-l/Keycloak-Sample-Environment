129b03e5bf80a1150ed8f00f4afb36b63cd21c48
Thorsten Ludewig <t.ludewig@gmail.com>
2023-08-23 10:42:12 +0200

    use xattr on macOS only

--- keycloak/bin/STARTUP.sh
+++ keycloak/bin/STARTUP.sh
@@ -4,7 +4,12 @@ docker compose down
 
 sudo rm -f kcldap/data/run/slapd-localhost.pid kcldap/data/run/slapd-localhost.socket
 sudo rm -fr kcdb/pg_stat_tmp
-sudo xattr -rc .
+
+if [ "Darwin" = `uname -s` ]; then
+  echo "Hello macOS user, cleaning up extended attributes."
+  sudo xattr -rc .
+  sleep 2
+fi
 
 docker compose up -d
 docker compose logs -f
